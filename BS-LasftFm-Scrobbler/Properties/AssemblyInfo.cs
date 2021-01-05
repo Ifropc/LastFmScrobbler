099519b85951a8e48f276d85ff61c567f1de8b81
Ifropc <ifropc@apache.org>
2021-01-04 20:34:59 -0800

    Mod initialization

---
+++ BS-LasftFm-Scrobbler/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BS-LasftFm-Scrobbler")]
+[assembly: AssemblyDescription("Lastf scrobbler for Beat Saber")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BS-LasftFm-Scrobbler")]
+[assembly: AssemblyCopyright("Copyright © Ifropc 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e8ea7eed-a4cb-4105-931f-ab1d15c3dbcc")]
+
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyFileVersion("0.0.1")]
---
+++ BS-LasftFm-Scrobbler/Utils/Logger.cs
@@ -0,0 +1,7 @@
+﻿namespace BS_LasftFm_Scrobbler.Utils
+{
+	internal class Logger
+	{
+		public static IPA.Logging.Logger log { get; set; }
+	}
+}
